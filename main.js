cd1d18dbc52fa43e250900002a5faedcd22f3736
Dhrati1410 <84124855+Dhrati1410@users.noreply.github.com>
2021-10-19 16:27:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+menu_list_array = ["Chicken Tandoori Pizza","Paneer Tikka Pizza","Deluxe Veggie Pizza","Veg Extravaganza Pizza","Veg Margherita Pizza",
+];    
+function getmenu(){
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+     menu_list_array.sort();
+
+    for( var i = 0; i < menu_list_array.length; i++) {
+    htmldata = htmldata + '<li>' + menu_list_array[i] + '</li>'}
+    htmldata = htmldata + "</ol>"
+
+    document.getElementById("display_menu").innerHTML = htmldata;
+};
+
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+
+        htmldata=htmldata+'<div class="card">' +'<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXrODHn7Z7myIAeZR848PO63i3sR_Aya_iLA&usqp=CAU" id="img_pizza">' + menu_list_array[i] + '</div>'
+    }
+      htmldata=htmldata+"</section>"
+      document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
